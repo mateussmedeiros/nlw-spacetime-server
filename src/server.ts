069f76180cdea66e94bfbb0f93969de8b6292343
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2023-05-18 18:02:36 -0300

    Add: autenticação

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
@@ -7,12 +11,17 @@ const app = fastify()
 app.register(cors, {
   origin: true,
 })
+app.register(jwt, {
+  secret: 'spacetime',
+})
 
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP server running on http://localhost:3333')
